00cdd373b2f54c6b86497640cce95d405e1500a9
Tri Nguyen <minhtri257tk@gmail.com>
2025-04-03 23:07:10 +0700

    no use strict mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import ChatRoom from './components/ChatRoom';
-import Login from './components/Login';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import AuthProvider from './Context/AuthProvider';
+import "./App.css";
+import ChatRoom from "./components/ChatRoom";
+import Login from "./components/Login";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import AuthProvider from "./Context/AuthProvider";
+import AppProvider from "./Context/AppProvider";
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ChatRoom />} />
-        </Routes>
+        <AppProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<ChatRoom />} />
+          </Routes>
+        </AppProvider>
       </AuthProvider>
     </BrowserRouter>
   );
